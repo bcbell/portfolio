93a95b258b40a37a3ecaa2aa70d22632b3596853
bcbell <coleman.britt@gmail.com>
2021-02-20 15:41:47 -0500

    Deploy

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 import environ
 environ.Env()
 environ.Env.read_env()
@@ -125,12 +126,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL='/about'
 
 LOGOUT_REDIRECT_URL='/'
 
-import django_on_heroku
 django_heroku.settings(locals())
